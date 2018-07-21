d3e2a6109cc17a9fb7cecc868ef95dc9f7f52bb8
meliodaseren-MacOSX <julian07027@gmail.com>
2018-07-21 11:01:17 +0800

    Add 001

---
+++ 001/plot_heart_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+y, x = np.ogrid[-2:2:100j, -2:2:100j]
+plt.contour(x.ravel(), y.ravel(), (x**2+y**2-1)**3 - x**2*y**3, [0])
+plt.savefig('./001/001_heart.png')
+plt.show()
