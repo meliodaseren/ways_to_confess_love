085fe8a81dfc9377e4afd5cb0008acec3d61a1c8
meliodaseren-MacOSX <julian07027@gmail.com>
2018-07-27 23:33:45 +0800

    Modify 002 code

--- 002/plot_heart_function.02.py
+++ 002/plot_heart_function.02.py
@@ -4,9 +4,10 @@ import sympy as syp
 import matplotlib.pyplot as plt
 
 syp.var('x y')
-graph = syp.plot_implicit((x**2+y**2-1)**3 - x**2*y**3, show=False)
+syp.plot_implicit((x**2+y**2-1)**3 - x**2*y**3)
 
-backend = graph.backend(graph)
-backend.backend(graph).process_series()
-backend.backend(graph).fig.savefig('./002/002_heart.png')
-backend.backend(graph).show()
+#graph = syp.plot_implicit((x**2+y**2-1)**3 - x**2*y**3, show=False)
+#backend = graph.backend(graph)
+#backend.backend(graph).process_series()
+#backend.backend(graph).fig.savefig('./002/002_heart.png')
+#backend.backend(graph).show()
